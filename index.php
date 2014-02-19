f76bf0fbcda4cf59fe1e19d08effddf4c85182ba
Zohirul Alam Tiemoon <TOSHIBA@TOSHIBA-PC>
2014-02-19 21:09:49 +0600

    update index.php

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ and open the template in the editor.
         <?php
             echo 'Hello world';
             echo 'Git test';
+            echo 'this is a tutorial on Git';
         ?>
     </body>
 </html>

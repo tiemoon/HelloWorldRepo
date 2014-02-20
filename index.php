434b7260fb2d7e2b9d1b67f550dac8aed285bdf7
Zohirul Alam Tiemoon <TOSHIBA@TOSHIBA-PC>
2014-02-20 14:37:13 +0600

    commit lower one

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ and open the template in the editor.
             echo 'Git test';
             echo 'this is a tutorial on Git';
             echo 'lower one';
+            echo 'upper one';
         ?>
     </body>
 </html>
